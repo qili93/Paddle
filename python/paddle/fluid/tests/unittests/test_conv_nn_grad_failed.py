6b90f528e4cd680cecd48f71ad18a2f84e8c49db
zhangkeliang <zhangkeliang@baidu.com>
2021-02-24 10:05:23 +0000

    add failed tests for test_conv_nn_grad

--- python/paddle/fluid/tests/unittests/test_conv_nn_grad_failed.py
+++ python/paddle/fluid/tests/unittests/test_conv_nn_grad_failed.py
@@ -57,5 +57,106 @@ class TestConvDoubleGradCheck(unittest.TestCase):
         paddle.disable_static()
 
 
+class TestConv3DDoubleGradCheck(unittest.TestCase):
+    @prog_scope()
+    def func(self, place):
+        shape = [2, 4, 3, 4, 2]
+        eps = 0.005
+        dtype = np.float64
+        if core.is_compiled_with_rocm():
+            dtype = np.float32
+        x = layers.data('x', shape, False, dtype)
+        y = layers.conv3d(x, 2, 1, bias_attr=False)
+        x_arr = np.random.uniform(-1, 1, shape).astype(dtype)
+
+        w = fluid.default_main_program().global_block().all_parameters()
+        w_arr = []
+        for p in w:
+            w_arr.append(np.random.uniform(-1, 1, p.shape).astype(dtype))
+        gradient_checker.double_grad_check(
+            [x] + w, y, x_init=[x_arr] + w_arr, place=place, eps=eps, atol=1e-3)
+
+    def test_grad(self):
+        paddle.enable_static()
+        places = [fluid.CPUPlace()]
+        if core.is_compiled_with_cuda():
+            places.append(fluid.CUDAPlace(0))
+        for p in places:
+            self.func(p)
+        paddle.disable_static()
+
+
+class TestConv2DoubleGradCheck_ChannelLast(unittest.TestCase):
+    @prog_scope()
+    def func(self, place):
+        shape = [2, 2, 3, 3]
+        eps = 0.005
+        dtype = np.float64
+        if core.is_compiled_with_rocm():
+            dtype = np.float32
+        x = layers.data('x', shape, False, dtype)
+        y = layers.conv2d(
+            input=x,
+            num_filters=2,
+            filter_size=1,
+            padding=[1, 1],
+            bias_attr=False,
+            use_cudnn=True,
+            groups=1,
+            data_format="NHWC")
+        x_arr = np.random.uniform(-1, 1, shape).astype(dtype)
+
+        w = fluid.default_main_program().global_block().all_parameters()
+        w_arr = []
+        for p in w:
+            w_arr.append(np.random.uniform(-1, 1, p.shape).astype(dtype))
+        gradient_checker.double_grad_check(
+            [x] + w, y, x_init=[x_arr] + w_arr, place=place, eps=eps, atol=1e-3)
+
+    def test_grad(self):
+        paddle.enable_static()
+        places = [fluid.CPUPlace()]
+        if core.is_compiled_with_cuda():
+            places.append(fluid.CUDAPlace(0))
+        for p in places:
+            self.func(p)
+        paddle.disable_static()
+
+
+class TestConv3DDoubleGradCheck_AsyPadding(unittest.TestCase):
+    @prog_scope()
+    def func(self, place):
+        shape = [2, 2, 2, 2, 2]
+        eps = 0.005
+        dtype = np.float64
+        if core.is_compiled_with_rocm():
+            dtype = np.float32
+        x = layers.data('x', shape, False, dtype)
+        y = layers.conv3d(
+            input=x,
+            num_filters=2,
+            filter_size=1,
+            padding=[1, 0, 0, 1, 1, 2],
+            bias_attr=False,
+            use_cudnn=True)
+        x_arr = np.random.uniform(-1, 1, shape).astype(dtype)
+
+        w = fluid.default_main_program().global_block().all_parameters()
+        w_arr = []
+        for p in w:
+            w_arr.append(np.random.uniform(-1, 1, p.shape).astype(dtype))
+        gradient_checker.double_grad_check(
+            [x] + w, y, x_init=[x_arr] + w_arr, place=place, eps=eps)
+
+    def test_grad(self):
+        paddle.enable_static()
+        places = [fluid.CPUPlace()]
+        if core.is_compiled_with_cuda():
+            places.append(fluid.CUDAPlace(0))
+        for p in places:
+            self.func(p)
+        paddle.disable_static()
+
+
 if __name__ == "__main__":
     unittest.main()
